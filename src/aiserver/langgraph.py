ffc2c8b6de5f8bddec19fec185a46d0e9c9aa8b8
Lucas Faé Baldan <lucasbaldan2014@gmail.com>
2025-05-05 23:04:33 -0300

    implementado a funcionalidade da IA identificar as alterações na memória, deletar e inserir uma nova para consulta. Ainda falta implementar a questão dos ids personalizados para pegar as memórias

--- src/aiserver/langgraph.py
+++ src/aiserver/langgraph.py
@@ -1,4 +1,5 @@
 import datetime
+import uuid
 from langgraph.prebuilt import create_react_agent # FunÃ§Ã£o principal do LangGraph para criar agentes ReAct
 from langchain_core.messages import SystemMessage # Para definir a mensagem de sistema (persona)
 from langchain_core.tools import tool # Decorador para definir ferramentas
@@ -21,7 +22,7 @@ else:
     documentos = [
         Document(
             page_content="O nome do setor responsável pela tecnologia da secretaria de educação é o Setor de Informática Educacional",
-            metadata={"origem": "sistema", "data": "2025-05-02", "tipo": "contato"}
+            metadata={"id":uuid.uuid4(), "origem": "sistema", "data": "2025-05-02", "tipo": "contato"}
         )
     ]
     faiss = FAISS.from_documents(documentos, embedding_model)
@@ -40,24 +41,58 @@ def salvar_info_faiss(texto: str, tipo: str):
     A IA deve inferir o tipo com base no conteúdo da informação.
     """
     documento = Document(
+        id=str(uuid.uuid4()),
         page_content=texto,
         metadata={
+            "id": str(uuid.uuid4()),
             "origem": "usuario",
             "tipo": tipo,
-            "data": datetime.now().strftime("%Y-%m-%d"),
+            "data": datetime.datetime.now().strftime("%Y-%m-%d")
         }
     )
     faiss.add_documents([documento])
     faiss.save_local(caminho_faiss)
 
 @tool
-def buscar_info_faiss(pergunta_usuario: str) -> list[str] | None:
-    """Busca as informação mais relevantes no índice FAISS com base na pergunta feita pelo usuário."""
+def buscar_info_faiss(pergunta_usuario: str) -> list[dict] | None:
+    """
+    Busca as informações mais relevantes no índice FAISS com base na pergunta feita pelo usuário.
+    Retorna uma lista de dicionários contendo conteúdo, id e metadados.
+    
+    """
     resultados = faiss.similarity_search(pergunta_usuario, k=3)
     if resultados:
-        return [r.page_content for r in resultados] 
+        return [
+            {
+                "id": r.metadata.get("id", 0),
+                "conteudo": r.page_content,
+                "metadados": r.metadata
+            }
+            for r in resultados
+        ]
     else:
         return None
+
+@tool
+def atualizar_info_faiss(doc_id: str, novo_texto: str, metadados: Document):
+    """
+    Exclui uma informação Salva no banco vetorial FAISS que está desatualizada e adiciona a nova informação atualização para uso futuro.
+
+    Parâmetros:
+    - doc_id: o ID do documento FAISS a ser deletado.
+    - texto: o conteúdo atualizado a ser registrado.
+    - metadados: reaproveitar os metadados do documento original, exceto o ID e data.
+
+    """
+    # Deletar o documento original
+    faiss.delete([doc_id])
+
+    metadados.id = str(uuid.uuid4())  # Gerar um novo ID para o documento atualizado
+    metadados["data"] = datetime.datetime.now().strftime("%Y-%m-%d")  # Atualizar a data
+    metadados.page_content = novo_texto  # Atualizar o conteúdo do documento
+
+    faiss.add_documents([metadados]) # Adicionar o novo documento ao índice
+    faiss.save_local(caminho_faiss)
 
 
 
@@ -69,17 +104,24 @@ model = ChatOpenAI(
 
 system_message = SystemMessage(content="""
                                             Você é o E-du, um agente de IA criado para atender aos usuários do sistema de gestão educacional da empresa EL.
+                                            
                                             Você deve responder de forma clara e objetiva. 
+                                            
                                             Sempre que o usuário fornecer uma informação nova e útil que possa ser usada para consultas futuras, salve a informação usando a ferramenta `salvar_info_faiss`, você deve:
                                             - Identificar o tipo da informação com uma das categorias: 'contato', 'estrutura', 'procedimento', 'evento', etc.
                                             - Preencher esse tipo corretamente no parâmetro 'tipo'.
+                               
+                                            Caso o usuário fornecer uma informação que está salva na base de dados, porém desatualizada, grave a nova usando a ferramenta `atualizar_info_faiss`, você deve:
+                                            - Enviar pelo parâmetro 'doc_id' o ID do documento que está desatualizado.
+                                            - Enviar pelo parâmetro 'novo_texto' a nova informação.
+                                            - Eviar pelo parâmetro 'metadados' os metadados do documento original.             
                                             
-                                            Se a resposta for muito longa, resuma-a e pergunte se o usuário quer mais informações.
+                                            Sempre que possível tente formatatar as respostas em Markdown, para facilitar a leitura.
                                             """)
 
 
 
-tools = [salvar_info_faiss, buscar_info_faiss] # Lista de ferramentas para o agente
+tools = [salvar_info_faiss, buscar_info_faiss, atualizar_info_faiss] # Lista de ferramentas para o agente
 
 graph = create_react_agent(
     model,
